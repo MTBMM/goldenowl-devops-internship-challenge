facadb337fee9b78dc422e6e4c05b9ed616dcfc6
MTBMM <kiennguyentrung408gmail.com>
2025-10-31 16:56:11 +0700

    kien

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
-        message: 'Welcome warriors to Golden Owl!, Nguyen Trung Kien',
+        message: 'Welcome warriors to Golden Owl!, Nguyen Trung Kien kien kien',
     }
     res.json(responseJson)
 })
